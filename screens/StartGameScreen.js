8ebbcf6edde3eb7da1756b253a75f723bf7312ce
shiv <shivsharma1990@gmail.com>
2020-04-16 15:46:09 +0530

    Input design and input validation

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Card from '../components/Card';
 import Colors from '../constants/colors';
+import Input from '../components/Input';
 
 const StartGameScreen = () => {
+	const [ enteredValue, setEnteredValue ] = useState('');
+
+	const numberInputHandler = (value) => {
+		setEnteredValue(value.replace(/[^0-9]/g, ''));
+	};
+
 	return (
+		<TouchableWithoutFeedback onPress={() => {
+			Keyboard.dismiss();
+		}}>
 		<View style={styles.screen}>
 			<Text style={styles.title}>Start a New Game</Text>
 			<Card style={styles.inputContainer}>
 				<Text>Select a Number</Text>
-				<TextInput />
+				<Input
+					style={styles.input2e}
+					blurOnSubmit
+					autoCapatalize="none"
+					autoCorrect={false}
+					keyboardType="number-pad"
+					maxLength={2}
+					onChangeText={numberInputHandler}
+					value={enteredValue}
+				/>
 				<View style={styles.buttonContainer}>
 					<View style={styles.button}>
 						<Button title="Reset" color={Colors.accent} />
@@ -20,6 +39,7 @@ const StartGameScreen = () => {
 				</View>
 			</Card>
 		</View>
+		</TouchableWithoutFeedback>
 	);
 };
 
@@ -47,6 +67,9 @@ const styles = StyleSheet.create({
 	},
 	button: {
 		width: 100
+	},
+	input: {
+		width: 50
 	}
 });
 
