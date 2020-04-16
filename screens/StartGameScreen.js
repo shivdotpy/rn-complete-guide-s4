2d9cd43b63dd70c790fcdf641f5bab219d7aac73
shiv <shivssharma1990@gmail.com>
2020-04-16 11:21:12 +0530

    basic header and screen component

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const StartGameScreen = () => {
+	return (
+		<View>
+			<Text />
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default StartGameScreen;
