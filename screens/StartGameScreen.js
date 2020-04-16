15b320624dc5a531e39fe5afa5da5e5800a8b7d6
shiv <shivsharma1990@gmail.com>
2020-04-16 15:58:44 +0530

    Inuput message to UI

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,39 +6,65 @@ import Input from '../components/Input';
 
 const StartGameScreen = () => {
 	const [ enteredValue, setEnteredValue ] = useState('');
+	const [ confirmed, setConfirmed ] = useState(false);
+	const [ selectedNumber, setSelectedNumber ] = useState(null);
 
 	const numberInputHandler = (value) => {
 		setEnteredValue(value.replace(/[^0-9]/g, ''));
 	};
 
+	const resetInputHandler = () => {
+		setEnteredValue('');
+		setConfirmed(false);
+	};
+
+	const confirmInputHandler = () => {
+		const chosenNumber = parseInt(enteredValue);
+		if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+			return;
+		}
+
+		setConfirmed(true);
+		setSelectedNumber(chosenNumber);
+		setEnteredValue('');
+	};
+
+	let confirmedOuput;
+	if (confirmed) {
+		confirmedOuput = <Text>Chosen Number: {selectedNumber}</Text>;
+	}
+
 	return (
-		<TouchableWithoutFeedback onPress={() => {
-			Keyboard.dismiss();
-		}}>
-		<View style={styles.screen}>
-			<Text style={styles.title}>Start a New Game</Text>
-			<Card style={styles.inputContainer}>
-				<Text>Select a Number</Text>
-				<Input
-					style={styles.input2e}
-					blurOnSubmit
-					autoCapatalize="none"
-					autoCorrect={false}
-					keyboardType="number-pad"
-					maxLength={2}
-					onChangeText={numberInputHandler}
-					value={enteredValue}
-				/>
-				<View style={styles.buttonContainer}>
-					<View style={styles.button}>
-						<Button title="Reset" color={Colors.accent} />
+		<TouchableWithoutFeedback
+			onPress={() => {
+				Keyboard.dismiss();
+			}}
+		>
+			<View style={styles.screen}>
+				<Text style={styles.title}>Start a New Game</Text>
+				<Card style={styles.inputContainer}>
+					<Text>Select a Number</Text>
+					<Input
+						style={styles.input2e}
+						blurOnSubmit
+						autoCapatalize="none"
+						autoCorrect={false}
+						keyboardType="number-pad"
+						maxLength={2}
+						onChangeText={numberInputHandler}
+						value={enteredValue}
+					/>
+					<View style={styles.buttonContainer}>
+						<View style={styles.button}>
+							<Button title="Reset" color={Colors.accent} onPress={resetInputHandler} />
+						</View>
+						<View style={styles.button}>
+							<Button title="Confirm" color={Colors.primary} onPress={confirmInputHandler} />
+						</View>
 					</View>
-					<View style={styles.button}>
-						<Button title="Confirm" color={Colors.primary} />
-					</View>
-				</View>
-			</Card>
-		</View>
+				</Card>
+				{confirmedOuput}
+			</View>
 		</TouchableWithoutFeedback>
 	);
 };
